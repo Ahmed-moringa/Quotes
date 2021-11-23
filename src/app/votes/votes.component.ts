c95b023da459412fa50d2c785461b8296b878b9e
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2021-11-23 19:07:29 +0300

    created downvote and upvote counters

--- src/app/votes/votes.component.ts
+++ src/app/votes/votes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-votes',
@@ -6,9 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./votes.component.css']
 })
 export class VotesComponent implements OnInit {
+  @Output() vote = new EventEmitter<number>();
+
+  totalUpVotes: number = 0;
+  totalDownVotes: number = 0;
 
   constructor() { }
-
+  upVote(){
+    this.totalUpVotes += 1
+    this.vote.emit(this.totalUpVotes) 
+  }
+  downVote(){
+    this.totalDownVotes += 1
+    this.vote.emit(this.totalDownVotes)
+  }
   ngOnInit(): void {
   }
 
