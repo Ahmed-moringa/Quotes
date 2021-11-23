d0557c561407ec8b057862cba5d1329f3042b77d
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2021-11-23 08:20:24 +0300

    added authors to quotes in quote component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,11 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
-    {id:1, name:'The purpose of our lives is to be happy'},
-    {id:2,name:'Life is what happens when you’re busy making other plans.'},
-    {id:3,name:'Get busy living or get busy dying.'},
-    {id:4,name:'You only live once, but if you do it right, once is enough.'},
-    {id:5,name:'Many of life’s failures are people who did not realize how close they were to success when they gave up.'},
+    {id:1, name:'The purpose of our lives is to be happy', author: 'Dalai Lama'},
+    {id:2,name:'Life is what happens when you’re busy making other plans.', author: 'John Lennon'},
+    {id:3,name:'Get busy living or get busy dying.', author: 'Stephen King'},
+    {id:4,name:'You only live once, but if you do it right, once is enough.', author: 'Mae West'},
+    {id:5,name:'Many of life’s failures are people who did not realize how close they were to success when they gave up.', author: 'Thomas A.Edison'},
   ];
   constructor() { }
 
