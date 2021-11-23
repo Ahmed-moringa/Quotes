2bc8ebb84c0ec5b1dbe090953fa4a8c71cd21398
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2021-11-23 19:43:50 +0300

    finalized project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { DateCountPipe } from './date-count.pipe';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { VotesComponent } from './votes/votes.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { VotesComponent } from './votes/votes.component';
     QuoteDetailComponent,
     DateCountPipe,
     QuoteFormComponent,
-    VotesComponent
+    VotesComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
