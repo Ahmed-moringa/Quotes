3472af7a17fb2d05e85c55c6d4b4f1f8a34bcef4
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2021-11-23 17:12:33 +0300

    fixed functionality of submition,

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -9,10 +9,10 @@ import { Quote } from '../quote';
 export class QuoteFormComponent implements OnInit {
   
   newQuote= new Quote(0, "", "", new Date());
-  @Output() addGoal = new EventEmitter<Quote>();
+  @Output() addQuote = new EventEmitter<Quote>();
   
-  submitGoal(){
-    this.addGoal.emit(this.newQuote);
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
   }
 
   constructor() { }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -17,6 +17,12 @@ export class QuoteComponent implements OnInit {
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
+  addNewQuote(quote){
+    let quoteLength = this.toggleDetails.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
   deleteQuote(isComplete, index){
     if(isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
