491e846304dc5dfbace5fb8dd9395db7d5f72670
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2021-11-23 08:14:59 +0300

    made added id and quotes to quote component and displayed it on app

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  quotes:Quote[] = [
+    {id:1, name:'The purpose of our lives is to be happy'},
+    {id:2,name:'Life is what happens when you’re busy making other plans.'},
+    {id:3,name:'Get busy living or get busy dying.'},
+    {id:4,name:'You only live once, but if you do it right, once is enough.'},
+    {id:5,name:'Many of life’s failures are people who did not realize how close they were to success when they gave up.'},
+  ];
   constructor() { }
 
   ngOnInit(): void {
