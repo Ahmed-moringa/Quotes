47a0a65793cae6a1d12dd1c70fece9e225925175
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2021-11-23 14:53:00 +0300

    added a last seen functionality

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
     showDescription:boolean;
 
-    constructor(public id: number, public name: string, public author: string){
+    constructor(public id: number, public name: string, public author: string, public completeDate: Date){
         this.showDescription= false;
     }
 }
